b3846ebd4d2c8cfb99302e6737e2cf7e273364c6
Johan Adriaans <johan.adriaans@gmail.com>
2010-07-22 08:49:01 +0000

    Changed MIN_PHPVER from 5.0.0 to 5.2.10. Applied deadeyes cleanrequestdata fix
    git-svn-id: http://php-transmission-class.googlecode.com/svn/trunk@13 f5a7c949-13a2-e6d6-403e-3b204bdbc950

--- class/TransmissionRPC.class.php
+++ class/TransmissionRPC.class.php
@@ -51,8 +51,9 @@ class TransmissionRPC
 
   /**
    * Minimum PHP version required
+   * 5.2.10 implemented the required http stream ignore_errors option
    */
-  const MIN_PHPVER = '5.0.0';
+  const MIN_PHPVER = '5.2.10';
 
   /**
    * The URL to the bittorent client you want to communicate with
@@ -345,7 +346,7 @@ class TransmissionRPC
     {
       if( is_object( $value ) ) $array[$index] = $value->toArray();	// Convert objects to arrays so they can be JSON encoded
       if( is_array( $value ) ) $array[$index] = $this->cleanRequestData( $value );	// Recursion
-      if( empty( $value ) ) unset( $array[$index] );	// Remove empty members
+      if( empty( $value ) && $value != 0 ) unset( $array[$index] );	// Remove empty members
       if( is_numeric( $value ) ) $array[$index] = $value+0;	// Force type-casting for proper JSON encoding (+0 is a cheap way to maintain int/float/etc)
       if( is_bool( $value ) ) $array[$index] = ( $value ? 1 : 0);	// Store boolean values as 0 or 1
       if( is_string( $value ) ) $array[$index] = utf8_encode( $value );	// Make sure all data is UTF-8 encoded for Transmission
